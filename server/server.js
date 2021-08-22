04b5e4e43de523a1b5de61f13a80c09bf57ead46
Danita Forbes <danitacodes@gmail.com>
2021-08-21 17:25:29 -0700

    trying to deploy

--- server/server.js
+++ server/server.js
@@ -19,9 +19,9 @@ app.use("/api/study", studyRoutes);
 app.use(errorHandler);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "index.html"));
   });
 }
 
