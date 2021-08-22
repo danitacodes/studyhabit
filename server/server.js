e308d4ced578805c7d6e2038de796ccd21169039
Danita Forbes <danitacodes@gmail.com>
2021-08-21 18:20:24 -0700

    reverting code

--- server/server.js
+++ server/server.js
@@ -18,13 +18,6 @@ app.use("/api/users", userRoutes);
 app.use("/api/study", studyRoutes);
 app.use(errorHandler);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "index.html"));
-  });
-}
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
