4e9036e07d8278af3c570d462b5dcbcd070ca99b
Danita Forbes <danitacodes@gmail.com>
2021-08-22 20:28:02 -0700

    app index store

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootswatch/dist/minty/bootstrap.min.css"; // Added this :boom:
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route } from "react-router-dom";
+import Home from "./screens/Home/Home";
+import Signin from "./screens/Signin/SigninScreen";
+import Signup from "./screens/Signup/Signup";
+import StudyList from "./screens/study/Studylist";
+import EditStudy from "./screens/Edit/editstudy";
+import StudyPage from "./screens/study/Studypage";
+import Profile from "./screens/Profile/ProfileScreen";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/studypage" component={StudyPage} />
+          <Route exact path="/study/:id" component={EditStudy} />
+          <Route exact path="/studylist" component={StudyList} />
+        </main>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
