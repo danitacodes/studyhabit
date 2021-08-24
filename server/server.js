c97a2fa03141451c569b45df947665b3c6e4c131
Danita Forbes <danitacodes@gmail.com>
2021-08-23 17:40:52 -0700

    trying to deploy

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const connectDB = require("./config/db");
 
 const userRoutes = require("./routes/userRoutes");
@@ -8,6 +9,7 @@ const errorHandler = require("./middleware/errorMiddleware");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 dotenv.config();
