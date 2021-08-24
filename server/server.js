b4bb144f007c21c87b08efa9092403c313af1fd1
Danita Forbes <danitacodes@gmail.com>
2021-08-23 19:03:48 -0700

    still working on deployment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 const userRoutes = require("./routes/userRoutes");
 const studyRoutes = require("./routes/studyRoutes");
@@ -15,13 +15,9 @@ app.use(express.json());
 dotenv.config();
 connectDB();
 
-app.use("https://studyhabit.herokuapp.com/api/users", userRoutes);
-app.use("https://studyhabit.herokuapp.com/api/study", studyRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/study", studyRoutes);
 app.use(errorHandler);
-
-app.get("/", (req, res) => {
-  res.send("Hello from Express");
-});
 
 const PORT = process.env.PORT || 5000;
 
